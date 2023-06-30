9d88cf9ce1f9a7f04ea8a132d9cffc8529e91ad3
Victoria Cruz <yvictoriacs@hotmail.com>
2023-06-30 13:28:14 -0600

    menu

--- user/menu.php
+++ user/menu.php
@@ -3,53 +3,39 @@ require_once("validar.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plan vivienda</title>
+    <link rel="icon" href="images/Mi proyecto.png">
+    <link rel="stylesheet" href="../user/estilos.css">
+    <a href="https://www.flaticon.es/iconos-gratis/estrella" title="estrella iconos"></a>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary"  data-bs-theme="white">
-  <div class="container-fluid">
-  <img class=" width="300px" height="100px" src="https://www.planvivienda.com.mx/img/logo01.png" alt>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarScroll">
-      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Link</a>
-        </li>
-      </ul>
-      <!-- <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="mx-3 btn btn-outline-success" type="submit">Search</button>
-      </form> -->
-      <!-- boton -->
-          <a class="mx-3 btn btn-outline-warning" href="../databases/cerrar.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
-              <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
-            </svg> Cerrar Sesion</a>
+    <div id="sidebar">
+        <div class="toggle-btn">
+            <span>&#9776;</span>
+        </div>
+
+       <li>
+            <img src="../images/Mi proyecto.png" alt="Logotipo" class="logo">
+            </li>
+
+        <center><a href="index.php"><img type="image" src="../images/correo-electronico.gif" alt="Logotipo" class="logo2"><span>Recibidos</span></a>
+
+        <center><a href="index.php"><img type="image" src="../images/estrella.gif" alt="Logotipo" class="logo2"><span>Destacados</span></a>
+
+        <center> <a href="index.php"><img type="image" src="../images/reloj.gif" alt="Logotipo" class="logo2"><span>Pospuestos</span></a>
+
+        <center><a href="index.php"><img type="image" src="../images/desfile-aereo.gif" alt="Logotipo" class="logo2"><span>Enviados</span></a></center>
+        <center><a href="index.php"><img type="image" src="../images/publicalo.gif" alt="Logotipo" class="logo2"><span>Enviados</span></a></center>
+
+         <center><a href="../databases/cerrar.php"><img src="../images/boton-de-encendido.gif" alt="Logotipo" class="logo2"><span> cerar sesion</a></center>
     </div>
-  </div>
-</nav>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-</body>
+
+    <script img src="../user/estilo.js" charset="utf-8"> </script>
+    </div>
+
 </html>
