57d1a5b475b553fc80ae7751401a841b08970fce
Victoria Cruz <yvictoriacs@hotmail.com>
2023-07-03 14:47:25 -0600

    modal y solicitud

--- asesor/index.php
+++ asesor/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once('../user/index.php');
 require_once('../user/menu.php');
 ?>
 <!DOCTYPE html>
@@ -8,97 +9,76 @@ require_once('../user/menu.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Consulta Usuario</title>
+    <title>Eliminar Usuario</title>
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="../sweetalert/dist/sweetalert2.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
 </head>
+
 <body>
     <div class="container">
         <div class="row" style="justify-content: center;">
             <div class="col-8" style="margin-top:3%;">
                 <div class="table-responsive bg-white shadow-lg rounded p-4">
-                    <h2 class="text-center mb-2">Afiliados</h2>
+                    <h2 class="text-center mb-2">Correos recibidos</h2>
+                    <?php
+                    require_once('modal.php');
+                    ?>
+
+
                     <table id="example" class="table table-striped table-hover" style="width:100%">
                         <thead class="table-dark">
                             <tr>
-                                <th scope="col">REF_PV</th>
-                                <th scope="col">REF_AHORRO</th>
-                                <th scope="col">ID ASESOR</th>
-                                <th scope="col">NOMBRES</th>
-                
-
+                                <th scope="col">Nº</th>
+                                <th scope="col">Correo recibido</th>
+                                <th scope="col">Editar</th>
+                                <th scope="col">Eliminar</th>
                             </tr>
                         </thead>
+
                         <tbody class="table-primary">
                             <?php
-                            require_once("../databases/conexion.php");
                             $i = 1;
-                            $conexion= new Conexion;
-                            $usuario = $conexion->conectar2()->query("SELECT REF_PV, REF_AHORRO, IdAsesor, Nombres FROM AFILIADO");
-                            if ($usuario->num_rows > 0) {
-                                while ($cons = $usuario->fetch_assoc()) {
                             ?>
-                                    <tr>
-                                        <th scope="row"><?php echo $i++; ?></th>
-                                        <td><?php echo $cons["REF_PV"]; ?></td>
-                                        <td><?php echo $cons["REF_AHORRO"]; ?> </td>
-                                        <td><?php echo $cons["IdAsesor"]; ?> </td>
-                                        <td><?php echo $cons["Nombres"]; ?> </td>
-                                    </tr>
-                            <?php
-                                }
-                            } else {
-                                echo "No existen usuarios";
-                            }
-                            ?>
+                            <tr>
+                                <th scope="row"><?php echo $i++; ?></th>
+                                <th></th>
+                                <div style="justify-content: center;">
+                                    <td><a class="text-dark text-center btn btn-warning"><svg
+                                                xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                <path fill-rule="evenodd"
+                                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                            </svg></a></td>
+                                    <td><a class="text-dark text-center btn btn-danger"><svg
+                                                xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                                            </svg></a></td>
+                                </div>
+                            </tr>
                         </tbody>
                     </table>
-                    <div class="mt-4">
-                        <center>
-                            <!-- inicio -->
-                            <a href="registrodeusuario.php" class="text-decoration-none text-dark">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-skip-start-circle-fill" viewBox="0 0 16 16">
-                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM9.71 5.093 7 7.028V5.5a.5.5 0 0 0-1 0v5a.5.5 0 0 0 1 0V8.972l2.71 1.935a.5.5 0 0 0 .79-.407v-5a.5.5 0 0 0-.79-.407z" />
-                                </svg>
-                            </a>
-                            <!-- anterior -->
-                            <a href="registrodeusuario.php" class="text-decoration-none text-dark">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-caret-left-square-fill" viewBox="0 0 16 16">
-                                    <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm10.5 10V4a.5.5 0 0 0-.832-.374l-4.5 4a.5.5 0 0 0 0 .748l4.5 4A.5.5 0 0 0 10.5 12z" />
-                                </svg>
-                            </a>
-                            <!-- Siguiente -->
-                            <a href="eliminarusuario.php" class="text-decoration-none text-dark">
-
-                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-caret-right-square-fill" viewBox="0 0 16 16">
-                                    <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.5 10a.5.5 0 0 0 .832.374l4.5-4a.5.5 0 0 0 0-.748l-4.5-4A.5.5 0 0 0 5.5 4v8z" />
-                                </svg>
-                            </a>
-                            <!-- final -->
-                            <a href="eliminarusuario.php" class="text-decoration-none text-dark">
-
-                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-skip-end-circle-fill" viewBox="0 0 16 16">
-                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407L9.5 8.972V10.5a.5.5 0 0 0 1 0v-5a.5.5 0 0 0-1 0v1.528L6.79 5.093z" />
-                                </svg>
-                            </a>
-                        </center>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
     </script>
     <script src="../sweetalert/dist/sweetalert2.all.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             $('#example').DataTable({
                 language: {
                     url: 'https://cdn.datatables.net/plug-ins/1.12.1/i18n/es-MX.json'
@@ -106,6 +86,7 @@ require_once('../user/menu.php');
             });
         });
     </script>
+
 </body>
 
 </html>
---
+++ asesor/modal.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Modal con PHP y Bootstrap</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <style>
+        .modal-lg {
+            max-width: 4cm !important; /* Ajusta el ancho del modal */
+        }
+        .modal-xl {
+            max-width: 700px !important; /* Ajusta el ancho del modal */
+        }
+        .modal-custom {
+            max-width: 300px !important; /* Ajusta el ancho del modal */
+            max-height: 100px !important; /* Ajusta la altura del modal */
+        }
+    </style>
+</head>
+<body>
+
+<!-- Botón que activa el modal -->
+<button type="button" class="btn btn-light" data-toggle="modal" data-target="#myModal"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+</svg> Redactar
+</button>
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myModalLabel">Título del Modal</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                 <!-- Contenido del modal...-->
+                 <div class="row mb-3">
+                        <label for="fecha" class="col-sm-2 col-form-label">Destinatario:</label>
+                        <div class="col-sm-10">
+                            <input type="text" maxlength="20" class="form-control" id="Titulo" name="Titulo" required>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="fecha" class="col-sm-2 col-form-label">Asunto:</label>
+                        <div class="col-sm-10">
+                            <input type="text" maxlength="20" class="form-control" id="Titulo" name="Titulo" required>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="Descripcion" class="col-sm-2 col-form-label">Descripción: </label>
+                        <div class="col-sm-10 form-floating">
+                            <textarea class="form-control" id="Descripción" name="Descripcion" style="height: 150px;" maxlength="500" required></textarea>
+                        </div>
+                    </div>
+                <?php
+                // Aquí puedes colocar el contenido dinámico generado con PHP
+                ?>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary">Guardar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
